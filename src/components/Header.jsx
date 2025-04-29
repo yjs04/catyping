1de254ab17a338baf75ee930e3289dd193e14e79
yjs04 <yjsss04@kakao.com>
2025-04-29 15:25:10 +0900

    add : setting btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import campaign from "../asset/svg/campaign.svg"
 import light_svg from "../asset/svg/light_mode.svg"
 import dark_svg from "../asset/svg/dark_mode.svg"
+import setting_btn from "../asset/svg/settings.svg";
 
 const Header = ({}) => {
     return (
@@ -11,6 +12,7 @@ const Header = ({}) => {
                 <a href="/sign-in">Sign in</a>
                 <button id="notice_btn"><img src={campaign} alt="notice_btn" /></button>
                 <button id="light_btn"><img src={light_svg} alt="light_mode_btn" /></button>
+                <button id="setting_btn"><img src={setting_btn} alt="setting_btn" /></button>
             </nav>
         </header>
     );
